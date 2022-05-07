5a036031d306a66126d16b706217e26726c9af00
Alejandro Morrison <nelsonmorrisonhidalgo0@gmail.com>
2022-05-07 11:08:35 -0400

    Initial Commit

---
+++ Popcorn/PopcornApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornApp.swift
+//  Popcorn
+//
+//  Created by Alejandro Morrison on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PopcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
